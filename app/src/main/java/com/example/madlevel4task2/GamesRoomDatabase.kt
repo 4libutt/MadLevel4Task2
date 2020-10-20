d7c02b009a9f9e09264a7aaa02fdc1f371ed7d4d
4libutt <ali@vergelijkgroep.nl>
2020-10-21 00:37:05 +0200

    added databaseRoom class

---
+++ app/src/main/java/com/example/madlevel4task2/GamesRoomDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.madlevel4task2
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Game::class], version = 1, exportSchema = false)
+abstract class GamesRoomDatabase : RoomDatabase() {
+
+    abstract fun gameDao(): GameDao
+
+    companion object {
+        private const val DATABASE_NAME = "GAMES_DATABASE"
+
+        @Volatile
+        private var gamesRoomDatabaseInstance: GamesRoomDatabase? = null
+
+        fun getDatabase(context: Context): GamesRoomDatabase? {
+            if (gamesRoomDatabaseInstance == null) {
+                synchronized(GamesRoomDatabase::class.java) {
+                    if (gamesRoomDatabaseInstance == null) {
+                        gamesRoomDatabaseInstance =
+                            Room.databaseBuilder(context.applicationContext,GamesRoomDatabase::class.java, DATABASE_NAME).build()
+                    }
+                }
+            }
+            return gamesRoomDatabaseInstance
+        }
+    }
+
+}
