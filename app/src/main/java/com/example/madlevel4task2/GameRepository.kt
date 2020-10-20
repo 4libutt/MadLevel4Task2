de9b3e54723b8e0e4fd26fce511d186cc164a371
4libutt <ali@vergelijkgroep.nl>
2020-10-21 01:04:57 +0200

    added repository class

---
+++ app/src/main/java/com/example/madlevel4task2/GameRepository.kt
@@ -0,0 +1,30 @@
+package com.example.madlevel4task2
+
+import android.content.Context
+
+class GameRepository(context: Context) {
+
+    private val gameDao: GameDao
+
+    init {
+        val database = GamesRoomDatabase.getDatabase(context)
+        gameDao = database!!.gameDao()
+    }
+
+    suspend fun getAllGames(): List<Game> {
+        return gameDao.getAllGames()
+    }
+
+    suspend fun insertGame(game: Game) {
+        gameDao.insertGame(game)
+    }
+
+    suspend fun deleteGame(game: Game) {
+        gameDao.deleteGame(game)
+    }
+
+    suspend fun deleteAllGames() {
+        gameDao.deleteAllGames()
+    }
+
+}
