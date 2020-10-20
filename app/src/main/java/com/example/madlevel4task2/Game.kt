9936327b2abfdce4311a9b6370175e0dc9b71a16
4libutt <ali@vergelijkgroep.nl>
2020-10-21 00:30:57 +0200

    made game class an entity

--- app/src/main/java/com/example/madlevel4task2/Game.kt
+++ app/src/main/java/com/example/madlevel4task2/Game.kt
@@ -1,10 +1,26 @@
 package com.example.madlevel4task2
 
 import androidx.annotation.DrawableRes
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "game_table")
 data class Game (
+
+    @ColumnInfo(name = "gameDate")
     var date : String,
+
+    @ColumnInfo(name = "computerChoice")
     @DrawableRes var computerChoice: Int,
+
+    @ColumnInfo(name = "userChoice")
     @DrawableRes var userChoice: Int,
-    var result: String
+
+    @ColumnInfo(name = "result")
+    var result: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
 )
