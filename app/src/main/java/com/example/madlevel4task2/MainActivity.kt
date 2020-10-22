4b39ae0ce0d2e480ca426afa54d61cf8c6833a28
4libutt <ali@vergelijkgroep.nl>
2020-10-22 18:07:00 +0200

    added rock, paper, scissors logic and image changes. also tried to fix the toolbar

--- app/src/main/java/com/example/madlevel4task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task2/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.madlevel4task2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val activeMenu = R.menu.menu_delete
+    private var activeMenu = R.menu.menu_history
+    private lateinit var navController: NavController
+    private lateinit var gameRepository: GameRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        navController = findNavController(R.id.nav_host_fragment)
+        toolbar.setNavigationOnClickListener {
+            startActivity(Intent(applicationContext, RPSOverviewFragment::class.java))
+            finish()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -25,7 +36,23 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.history_icon -> true
+            R.id.history_icon -> {
+                item.isVisible = false
+                supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+                supportActionBar!!.setDisplayShowHomeEnabled(true)
+                activeMenu = R.menu.menu_delete
+                onCreateOptionsMenu(toolbar.menu)
+                toolbar.title = "Your Game History"
+                navController.navigate(
+                    R.id.action_RPSOverviewFragment_to_GameHistoryFragment
+                )
+                return true
+            }
+
+            R.id.delete_icon -> {
+
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
--- app/src/main/java/com/example/madlevel4task2/RPSOverviewFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/RPSOverviewFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_rps_overview.*
+import kotlinx.android.synthetic.main.item_game.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class RPSOverviewFragment : Fragment() {
+    var userMove = "paper"
+    var computerMove = "rock"
+
+    private lateinit var gameRepository: GameRepository
+
+    private val games = arrayListOf<Game>()
+    private val gameAdapter = GameAdapter(games)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +31,89 @@ class RPSOverviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        gameRepository = GameRepository(requireContext())
+
+        ivRock.setOnClickListener {
+            checkGameResult(ivRock)
+        }
+
+        ivPaper.setOnClickListener {
+            checkGameResult(ivPaper)
+        }
+
+        ivScissors.setOnClickListener {
+            checkGameResult(ivScissors)
+        }
+    }
+
+    private fun checkGameResult(view: View) {
+        userMove = view.contentDescription.toString()
+        generatePcMove()
+
+
+        when(userMove) {
+            "rock" -> {
+                ivUserChoice.setImageResource(R.drawable.rock)
+                when(computerMove){
+                    getString(R.string.rock) -> {
+                        tvResults.text = getString(R.string.draw)
+                    }
+                    getString(R.string.paper) -> {
+                        tvResults.text = getString(R.string.loss)
+                    }
+                    getString(R.string.scissors) -> {
+                        tvResults.text = getString(R.string.win)
+                    }
+                }
+            }
+            "paper" -> {
+                ivUserChoice.setImageResource(R.drawable.paper)
+                when(computerMove){
+                    getString(R.string.rock) -> {
+                        tvResults.text = getString(R.string.win)
+                    }
+                    getString(R.string.paper) -> {
+                        tvResults.text = getString(R.string.draw)
+                    }
+                    getString(R.string.scissors) -> {
+                        tvResults.text = getString(R.string.loss)
+                    }
+                }
+            }
+            "scissors" -> {
+                ivUserChoice.setImageResource(R.drawable.scissors)
+                when(computerMove){
+                    getString(R.string.rock) -> {
+                        tvResults.text = getString(R.string.loss)
+                    }
+                    getString(R.string.paper) -> {
+                        tvResults.text = getString(R.string.win)
+                    }
+                    getString(R.string.scissors) -> {
+                        tvResults.text = getString(R.string.draw)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun generatePcMove(){
+        val pcmove = (1..3).random()
+
+        when(pcmove) {
+            1 -> {
+                computerMove = "rock"
+
+                ivComputerChoice.setImageResource(R.drawable.rock)
+            }
+            2 -> {
+                computerMove = "paper"
+                ivComputerChoice.setImageResource(R.drawable.paper)
+            }
+            3 -> {
+                computerMove = "scissors"
+                ivComputerChoice.setImageResource(R.drawable.scissors)
+            }
+        }
     }
 }
